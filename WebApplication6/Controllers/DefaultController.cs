006a42d7c1cbc884bf3f1524fa2ac2733900de83
vu tri toan <vutritoan15933@gmail.com>
2020-03-22 16:25:27 +0900

    d

---
+++ WebApplication6/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Default()
+        {
+            ViewBag.View = "day la trang chu ";
+            return View();
+        }
+    }
+}
